6cb76571428e7937b4e2ab25aa8d3fbc45160074
Tyler Rutt <Tylerrutt17@gmail.com>
2020-09-09 14:50:36 -0400

    update

--- src/components/social/Search.js
+++ src/components/social/Search.js
@@ -2,11 +2,11 @@ import React, {useState, useEffect} from 'react';
 import '../../css/social/Search.css'
 import image from '../../images/search.png'
 
-function Search() {
+function Search({onChange}) {
     //const [email, setEmail] = useState('')
     return (
         <div>
-            <input className={'search'} placeholder={'Search'}></input>
+            <input className={'search'} placeholder={'Search'} onChange={onChange}></input>
             {/* <img src={image} className={'search-icon'} /> */}
         </div>
     )
